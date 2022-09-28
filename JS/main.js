575ff30e16f0c252733361869bde37de2f9f0745
Javier Tintaya <jhosimart42@gmail.com>
2022-09-27 21:45:22 -0400

    componente 04 shopping-cart fue agregado

--- JS/main.js
+++ JS/main.js
@@ -1,15 +1,50 @@
+//*Componente 01 y 02
 const menuEmail = document.querySelector(".nbRight__email");
 const dtMenu = document.querySelector(".desktopMenu");
 
-menuEmail.addEventListener("click", toggleDesktopMenu);
-function toggleDesktopMenu() {
-  dtMenu.classList.toggle("active");
-}
+// menuEmail.addEventListener("click", toggleDesktopMenu);
+// function toggleDesktopMenu() {
+//   dtMenu.classList.toggle("active");
+// }
+menuEmail.addEventListener("click", () => {
+  const isAsideClosed = aside.classList.contains("active");
+  if (!isAsideClosed) {
+    aside.classList.add("active");
+  }
 
+  dtMenu.classList.toggle("active");
+});
+
+//*Componente 03
 const hamBtn = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobileMenu");
 
-hamBtn.addEventListener("click", toggleMobileMenu);
-function toggleMobileMenu() {
+// hamBtn.addEventListener("click", toggleMobileMenu);
+// function toggleMobileMenu() {
+//   mobileMenu.classList.toggle("active");
+// }
+hamBtn.addEventListener("click", () => {
+  const isAsideClosed = aside.classList.contains("inactive");
+  if (!isAsideClosed) {
+    aside.classList.add("active");
+  }
   mobileMenu.classList.toggle("active");
-}
+});
+
+//*Componente 04
+const menuCarritoIcon = document.querySelector(".nbShoppingCart");
+const aside = document.querySelector(".product-detail");
+
+menuCarritoIcon.addEventListener("click", () => {
+  const isMobileMenuClosed = mobileMenu.classList.contains("active");
+  if (!isMobileMenuClosed) {
+    mobileMenu.classList.add("active");
+  }
+  aside.classList.toggle("active");
+
+  const isDesktopMenuClosed = dtMenu.classList.contains("active");
+  if (!isDesktopMenuClosed) {
+    dtMenu.classList.add("active");
+  }
+  // aside.classList.toggle("active");
+});
